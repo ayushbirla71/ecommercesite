f2c25f0c39a157b2986bb741073b411f73aa9225
ayushbirla71 <114582571+ayushbirla71@users.noreply.github.com>
2023-03-19 15:25:52 +0530

    update

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-let host='http://localhost:3001'
+// let host='http://localhost:3001';
+
+let host="https://ecommercesite-g3hwk1a7x-ayushbirla71.vercel.app/"
 
 export const fetchDataFromApi = async (obj) => {
 
